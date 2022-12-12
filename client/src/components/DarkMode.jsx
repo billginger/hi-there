47fdf0789cc6810028124f015f037fb3393c9ef9
Fenggong Jiang <jfg@163.com>
2022-12-12 14:01:14 +0800

    Update darkMode

--- client/src/components/DarkMode.jsx
+++ client/src/components/DarkMode.jsx
@@ -1,32 +1,36 @@
-import { useEffect, useState } from 'react';
+import { BsSun, BsMoon } from "react-icons/bs";
 
-const DarkMode = () => {
-  const [darkMode, setDarkMode] = useState(localStorage.darkMode);
-  const switchDarkMode = () => {
-    if (!darkMode) {
-      localStorage.darkMode = true;
-      setDarkMode(true);
-    } else {
-      localStorage.removeItem('darkMode');
-      setDarkMode(false);
-    }
-  };
-
-  const checkDarkMode = () => {
-    if (!darkMode) {
-      document.documentElement.classList.add('dark');
-    } else {
-      document.documentElement.classList.remove('dark');
-    }
+const checkDarkMode = () => {
+  if (localStorage.darkMode == 'on') {
+    document.documentElement.classList.add('dark');
+  } else {
+    document.documentElement.classList.remove('dark');
   }
-
-  useEffect(() => {
-    checkDarkMode();
-  }, [darkMode]);
-
-  return (
-    <div onClick={switchDarkMode}>Dark Mode</div>
-  );
 }
+
+checkDarkMode();
+
+const switchDarkMode = value => {
+  localStorage.darkMode = value;
+  checkDarkMode();
+};
+
+const DarkMode = () => (
+  <li className="flex border border-gray-600 rounded">
+    <div
+      className="p-1 text-white bg-gradient-to-br from-gray-600 to-violet-600 dark:bg-none dark:cursor-pointer"
+      onClick={() => switchDarkMode('off')}
+    >
+      <BsSun />
+    </div>
+    <div
+      className="p-1 text-gray-800 cursor-pointer dark:text-white dark:bg-gradient-to-br dark:from-gray-600
+        dark:to-violet-800 dark:cursor-auto"
+      onClick={() => switchDarkMode('on')}
+    >
+      <BsMoon />
+    </div>
+  </li>
+);
 
 export { DarkMode };
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,8 @@ const MenuItem = ({ name }) => (
   <li>
     <a
       href={`#${name}`}
-      className="text-gray-800 dark:text-violet-50 hover:text-violet-500 dark:hover:text-violet-400"
+      className="text-base font-semibold text-gray-800 dark:text-violet-50 hover:text-violet-500
+        dark:hover:text-violet-400 transition duration-300"
     >
       {name}
     </a>
@@ -16,15 +17,15 @@ const MenuItem = ({ name }) => (
 
 const Navbar = () => {
   return (
-    <div className="navbar dark:bg-gray-800">
+    <div className="navbar bg-white dark:bg-gray-800">
       <div className="container mx-auto p-2 flex justify-between">
         <span className="logo">Hi there</span>
         <ul className="hidden md:flex flex-1 ml-32 max-w-screen-md list-none flex justify-between items-center">
           {MenuItems.map((item, index) => (
             <MenuItem key={item + index} name={item} />
           ))}
+          <DarkMode />
         </ul>
-        <DarkMode />
       </div>
     </div>
   );
