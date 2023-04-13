f7b745204db167848a45f68b9b886d28dd2901ea
Fenggong Jiang <jfg@163.com>
2023-04-13 22:11:30 +0800

    Deploy Contract

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/vEdtuwQrMc9Ptj774JiDEMeKvEY3y2DC',
+      accounts: ['account2'],
+    }
+  }
 };
